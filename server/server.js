b74e6b59e1738d82cc75733681660867432b60f2
Sumanth-2305 <sumanthbitra321@gmail.com>
2026-02-01 20:45:09 +0530

    Added dynamic port for deployment

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import { operations, addOperation, undo, redo } from "./state-manager.js";
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("client"));
 
@@ -49,9 +50,9 @@ socket.on("redo", () => {
 
 
 
-
 });
 
-server.listen(3000, () => {
-  console.log("server running on http://localhost:3000");
+
+server.listen(PORT, () => {
+  console.log(`server running on ${PORT}`);
 });
